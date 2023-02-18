8aa59c63d487e059639d718c707d54a8e0014b47
Lucca Mazzucatto Lino <luccamlino5@gmail.com>
2023-02-17 21:36:59 -0300

    finalizei a aula sobre tipos primitivos

---
+++ ex003/index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tipos primitivos em PHP</title>
+</head>
+<body>
+    <h1>Teste de tipos primitivos</h1>
+    <?php 
+        // 0x = hexadecimal; 0b = binário; 0 = octal; 3e2 = 300 'notação científica'
+        // $num = 300;
+        // echo "O valor da variável é $num";
+
+        // $v = true;
+        // var_dump($v)
+
+
+        // $num = (int) "950"; // coerção
+        // var_dump($num)
+
+        // $vet = [6, 2.5, "Maria", 3, false];
+        // var_dump($vet)
+
+        // class Pessoa {
+        //     private string $nome;
+        // }
+        // $p = new Pessoa;
+        // var_dump($p)
+
+        
+    ?>
+</body>
+</html>
