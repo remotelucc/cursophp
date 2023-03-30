294adb82cabfcb4b6fcbca7c35a68262c0a0b582
Lucca Mazzucatto Lino <luccamlino5@gmail.com>
2023-03-30 17:27:41 -0300

    esqueci de dar o commit ;/

--- desafios/d04/conversao.php
+++ desafios/d04/conversao.php
@@ -10,11 +10,20 @@
 <body>
     <main>
         <h1>Conversor de moedas 2.0</h1>
+        <pre>
         <?php 
+        $inicio = date("m-d-Y", strtotime("-7 days"));
+        $fim = date("m-d-Y");
         $dinheiro = $_POST["dinheiro"];
+        $url = 'https://olinda.bcb.gov.br/olinda/servico/PTAX/versao/v1/odata/CotacaoDolarPeriodo(dataInicial=@dataInicial,dataFinalCotacao=@dataFinalCotacao)?@dataInicial=\''. $inicio .'\'&@dataFinalCotacao=\''. $fim .'\'&$top=1&$orderby=dataHoraCotacao%20desc&$format=json&$select=cotacaoCompra';
+        $dados = json_decode(file_get_contents($url), true);
+        $cotacao = $dados["value"][0]["cotacaoCompra"];
+        $dolar = $dinheiro / $cotacao;
 
-        echo "<p>Seus R$ $dinheiro equivalem a <strong>US$ $dolar</strong>*</p>";
+        $padrao = numfmt_create("pt_BR", NumberFormatter::CURRENCY);
+        echo "Seus " . numfmt_format_currency($padrao, $dinheiro, "BRL") . " equivalem a " . numfmt_format_currency($padrao, $dolar, "USD");
         ?>
+        </pre>
         <p>*Cotação obtida diretamente do site do <a href="https://olinda.bcb.gov.br/olinda/servico/PTAX/versao/v1/aplicacao#!/recursos/CotacaoDolarPeriodo#eyJmb3JtdWxhcmlvIjp7IiRmb3JtYXQiOiJqc29uIiwiJHRvcCI6MTAwfX0=">Banco Central do Brasil</a></p>
         <button><a href="index.html">Voltar</a></button>
     </main>
