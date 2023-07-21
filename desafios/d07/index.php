ce1551e4148bd65ae26cf33002a7d16fa7093a91
Lucca Mazzucatto Lino <luccamlino5@gmail.com>
2023-07-21 00:44:02 -0300

    melhorei o desafio 7

--- desafios/d07/index.php
+++ desafios/d07/index.php
@@ -9,15 +9,16 @@
 </head>
 <body>
     <?php 
-    $total = $_REQUEST['salario'] ?? 0;
-    $minimo = 1380;
+    $minimo = 1320;
+    $total = $_REQUEST['salario'] ?? $minimo;
+    $padrao = numfmt_create("pt_BR", NumberFormatter::CURRENCY);
     ?>
     <main>
         <h1>Informe seu salário</h1>
         <form action="<?= $_SERVER['PHP_SELF'] ?>" method="get">
         <label for="salario">Salário (R$)</label>
-        <input type="number" name="salario" id="salario">
-        <p>Considerando o salário mínimo de <strong>R$1.380,00</strong></p>
+        <input type="number" name="salario" id="salario" step="0.01" value="<?= $total ?>">
+        <p>Considerando o salário mínimo de <strong><?= numfmt_format_currency($padrao, $minimo, "BRL") ?></strong></p>
         <input type="submit" value="Calcular">
         </form>
     </main>
@@ -25,9 +26,8 @@
         <h2>Resultado final</h2>
         <?php 
         $quantia = intdiv($total, $minimo);
-        $resto = $total % $minimo;
-        $padrao = numfmt_create("pt_BR", NumberFormatter::CURRENCY);
-        echo"<p>Quem recebe um salario de " . numfmt_format_currency($padrao, $total, "BRL") . " ganha <strong>" . numfmt_format_currency($padrao, $quantia, "BRL") . " salário(s) mínimo(s) </strong>+ " . numfmt_format_currency($padrao, $resto, "BRL") . "</p>"
+        $resto = $total - ($quantia * $minimo);
+        echo"<p>Quem recebe um salario de " . numfmt_format_currency($padrao, $total, "BRL") . " ganha <strong> $quantia salário(s) mínimo(s) </strong>+ " . numfmt_format_currency($padrao, $resto, "BRL") . "</p>"
         ?>
     </section>
 </body>
